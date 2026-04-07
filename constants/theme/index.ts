04e3a3c463b93fde1031e39fe98dafcc081503f3
JKW1008 <todendvv91@gmail.com>
2026-04-07 16:43:00 +0900

    feat: initial app structure and design system (Issue #1)

---
+++ constants/theme/index.ts
@@ -0,0 +1,25 @@
+import { Colors } from './colors';
+import { Typography } from './typography';
+
+export const Theme = {
+  colors: Colors,
+  typography: Typography,
+  
+  spacing: {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    gutter: 16,
+  },
+
+  borderRadius: {
+    none: 0,
+    sm: 4,
+    md: 8,
+    lg: 16,
+  }
+};
+
+export default Theme;
